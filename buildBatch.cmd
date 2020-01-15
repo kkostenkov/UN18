2cd3a98274197ea2fcf8ac2c069f36007b429b7f
Kirill Kostenkov <kirill.kostenkov@heliogames.com>
2020-01-15 16:07:55 +0300

    fixed project version

--- buildBatch.cmd
+++
@@ -1,1 +0,0 @@
-CALL "C:\Program Files\Unity\2018.4.12f1\Editor\Unity.exe" -batchmode -quit -executeMethod Builder.Build -projectPath  "P:\Repos\UN18\UN18_Unity" -buildTarget iOS
---
+++ buildBatch_iOS.cmd
@@ -0,0 +1,1 @@
+CALL "C:\Program Files\Unity\2018.4.12f1\Editor\Unity.exe" -batchmode -quit -projectPath  "P:\Repos\UN18\UN18_Unity" -executeMethod Builder.Build 
