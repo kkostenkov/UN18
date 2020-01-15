fb1ef5040591746009085550d787c966ef46eded
Kirill Kostenkov <kirill.kostenkov@heliogames.com>
2020-01-15 16:12:16 +0300

    win build script fixed

--- buildBatch_win.cmd
+++ buildBatch_win.cmd
@@ -1,1 +1,1 @@
-CALL "C:\Program Files\Unity\2018.4.12f1\Editor\Unity.exe" -batchmode -quit -projectPath  "P:\Repos\UN18\UN18_Unity" -buildWindowsPlayer "P:\Builds"
+CALL "C:\Program Files\Unity\2018.4.12f1\Editor\Unity.exe" -batchmode -quit -projectPath  "P:\Repos\UN18\UN18_Unity" -buildWindowsPlayer "P:\Builds\UN18.exe"
