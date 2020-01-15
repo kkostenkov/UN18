9171ca387b93f78cc5f45471a9607436e7e253f5
Kirill Kostenkov <kirill.kostenkov@heliogames.com>
2020-01-15 15:52:40 +0300

    removed packages, added batch build script

---
+++ UN18_Unity/Assets/Builder.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+using UnityEditor;
+
+public class Builder 
+{
+    static void Build ()
+    {
+        var buildOpts = new BuildPlayerOptions()
+        {
+            options = BuildOptions.None,
+            target = BuildTarget.iOS,
+            targetGroup = BuildTargetGroup.iOS,
+            scenes = EditorBuildSettings.scenes
+                .Where(x => x.enabled)
+                .Select(x => x.path)
+                .ToArray(),
+//            locationPathName = prefs.FullBuildPath
+        };
+        var buildReport = BuildPipeline.BuildPlayer(buildOpts);
+    }
+}
+
